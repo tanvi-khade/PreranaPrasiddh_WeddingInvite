4f0e79bb2cf94874aecd0964a390bb6278ab82ae
Tanvi Khade <50665853+tanvi-khade@users.noreply.github.com>
2026-02-08 02:05:29 +0530

    RSVP

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+window.onload = function () {
+	var padding = document.getElementById("lavendar").clientHeight;
+	console.log("padding", padding);
+	if (padding > 79)
+		document.getElementById("rsvp").style.paddingTop = `${padding}px`;
+  else
+    document.getElementById("rsvp").style.paddingTop ='50px';
+}
+
 var span = document.getElementsByClassName("close")[0];
 var modal;
 function openPopup(eventName) {
@@ -16,34 +25,37 @@ window.onclick = function (event) {
 	}
 };
 
-
 var form = document.getElementById("rsvp-form");
-  
-  async function handleSubmit(event) {
-    event.preventDefault();
-    var status = document.getElementById("my-form-status");
-    var data = new FormData(event.target);
-    fetch(event.target.action, {
-      method: form.method,
-      body: data,
-      headers: {
-          'Accept': 'application/json'
-      }
-    }).then(response => {
-      if (response.ok) {
-        status.innerHTML = "Thanks for your submission!";
-        form.reset()
-      } else {
-        response.json().then(data => {
-          if (Object.hasOwn(data, 'errors')) {
-            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-          } else {
-            status.innerHTML = "Oops! There was a problem submitting your form"
-          }
-        })
-      }
-    }).catch(error => {
-      status.innerHTML = "Oops! There was a problem submitting your form"
-    });
-  }
-  form.addEventListener("submit", handleSubmit)
+
+async function handleSubmit(event) {
+	event.preventDefault();
+	var status = document.getElementById("my-form-status");
+	var data = new FormData(event.target);
+	fetch(event.target.action, {
+		method: form.method,
+		body: data,
+		headers: {
+			Accept: "application/json",
+		},
+	})
+		.then((response) => {
+			if (response.ok) {
+				status.innerHTML = "Thanks for your submission!";
+				form.reset();
+			} else {
+				response.json().then((data) => {
+					if (Object.hasOwn(data, "errors")) {
+						status.innerHTML = data["errors"]
+							.map((error) => error["message"])
+							.join(", ");
+					} else {
+						status.innerHTML = "Oops! There was a problem submitting your form";
+					}
+				});
+			}
+		})
+		.catch((error) => {
+			status.innerHTML = "Oops! There was a problem submitting your form";
+		});
+}
+form.addEventListener("submit", handleSubmit);
